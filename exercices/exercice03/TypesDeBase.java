188c9f973954f6a49189b0bd1be23292ed06ef1f
Ksawier Paszkowski <ksawieradam.paszkowski@studentfr.ch>
2024-08-29 11:48:18 +0200

    test

---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,16 @@
+package exercices.exercice03;
+
+/**
+ * TypesDeBase
+ */
+public class TypesDeBase {
+    public static void main(String[] args) {
+        int age = 16;
+        String name = "Ksawier";
+        System.out.println("Bonjour je m'appelle " + name + " et j'ai " + age + " ans");
+
+        int age10 = age+10;
+        System.out.println( "Et dans 10 ans j'aurais "+ age10 + " ans");
+    }
+
+}
