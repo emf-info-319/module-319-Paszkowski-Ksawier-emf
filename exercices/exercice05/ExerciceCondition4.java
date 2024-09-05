a5fe7f578f8175eb8efc3e3c0dc9c8004713c1a7
Ksawier Paszkowski <ksawieradam.paszkowski@studentfr.ch>
2024-09-05 10:58:53 +0200

    tst

--- exercices/exercice05/ExerciceCondition4.java
+++ exercices/exercice05/ExerciceCondition4.java
@@ -6,15 +6,14 @@ public class ExerciceCondition4 {
 
         if (temperature < -10)
                 System.out.println("Il fait très froid");
-        else if (temperature > -10 && temperature < 0)
+        else if (temperature >= -10 && temperature < 0)
                 System.out.println("Il fait froid");
-        else if (temperature >0 && temperature <25) 
+        else if (temperature >= 0 && temperature <25) 
                 System.out.println("Il fait normal");
-        else if (temperature >25 && temperature <35 ) 
+        else if (temperature >= 25 && temperature <35 ) 
                 System.out.println("Il fait chaud");
-        else if (temperature >35) 
+        else if (temperature >= 35) 
                 System.out.println("Il fait très chaud"); {
-
         }
     }
 }
