8106fe98ecb1df737b0d7a70e3ef039ea6c3dea6
Ksawier Paszkowski <ksawieradam.paszkowski@studentfr.ch>
2024-09-05 09:50:12 +0200

    test

---
+++ exercices/exercice05/ExerciceCondition3.java
@@ -0,0 +1,12 @@
+package exercices.exercice05;
+
+public class ExerciceCondition3 {
+    public static void main(String[] args) {
+        int valeur1 = -15;
+        int valeur2 = -10;
+
+        if (valeur1 > 0 & valeur2 > 0 ) System.out.println("La résultat est positif");
+        if (valeur1 < 0 & valeur2 < 0) System.out.println("La résultat est positif");
+        else System.out.println("Le résultat est négatif ");
+    }
+}
