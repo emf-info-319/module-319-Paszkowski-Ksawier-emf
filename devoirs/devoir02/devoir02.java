244a437c409ee5e5dcb250585096437bb430fe4e
Ksawier Paszkowski <ksawieradam.paszkowski@studentfr.ch>
2024-09-12 11:45:19 +0200

    tst

---
+++ devoirs/devoir02/devoir02.java
@@ -0,0 +1,13 @@
+package devoirs.devoir02;
+
+public class devoir02 {
+    public final static int CAPACITE_RESERVOIR_A = 3;
+    public final static int CAPACITE_RESERVOIR_B = 5;
+    public static void main(String[] args) {
+        int reservoirA = 0;
+        int reservoirB = 0;
+        int remplissage = (int) (Math.random() * (8 - 1 + 1) ) + 1 ;
+            System.out.println("il y a 8 litres à remplir");
+        
+    }
+}
