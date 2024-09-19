a812ddbcdd831b1fe6ef9bb942facce0eeb1235c
Ksawier Paszkowski <ksawieradam.paszkowski@studentfr.ch>
2024-09-19 09:00:58 +0200

    t

--- devoirs/devoir02/devoir02.java
+++ devoirs/devoir02/devoir02.java
@@ -6,14 +6,14 @@ public class devoir02 {
     public static void main(String[] args) {
         int reservoirA = 0;
         int reservoirB = 0;
-        int remplissage = (int) (Math.random() * (8 - 1 + 1) ) + 1 ;
-            System.out.println("il y a 8 litres à remplir");
+        int remplissage = (int) (Math.random() * (CAPACITE_RESERVOIR_A + CAPACITE_RESERVOIR_B - 1 + 1) ) + 1 ;
+            System.out.println("il y a " + CAPACITE_RESERVOIR_A + CAPACITE_RESERVOIR_B + " litres à remplir");
         while (reservoirA < 3) {
             System.out.println("Remplissage du reservoir A...");
             ++reservoirA;
             System.out.println("Le reservoir A : " + reservoirA + ", le reservoir B : " + reservoirB);
-        }
-        while (reservoirB < 2) {
+        }d
+        while (reservoirB < 5) {
             System.out.println("Remplissage du reservoir B...");
             ++reservoirB;
             System.out.println("Le reservoir A : " + reservoirA + ", le reservoir B : " + reservoirB);
